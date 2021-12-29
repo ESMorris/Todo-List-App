438099448577b45bc9b6f6a8203641351f14e7fc
simplydoesnot <emorris.mxi@gmail.com>
2021-12-28 23:27:00 -0800

    Added Mixins to restrict certain pages

--- TodoListApp/TodoApp/views.py
+++ TodoListApp/TodoApp/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Task
 
@@ -19,7 +20,7 @@ class CustomLoginView(LoginView):
         return reverse_lazy('tasks')
 
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
